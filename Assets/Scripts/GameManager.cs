6e1609d4dde7c2ad27a2f0a785a01070c66281e4
bayuRP <13513046@std.stei.itb.ac.id>
2016-04-20 09:26:41 +0700

    ui assets added, main menu added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GlobalData.player_name = PlayerPrefs.GetString ("player_name", "Player");
+		GlobalData.player_exp = (uint) (int) PlayerPrefs.GetInt ("player_exp", 0);
+		GlobalData.player_level = (uint) (int) PlayerPrefs.GetInt ("player_level", 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+}
---
+++ Assets/Scripts/GlobalData.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class GlobalData {
+	public static string player_name;
+
+	public static uint player_level;
+	public static uint player_exp;
+	public static uint next_exp;
+
+	public static uint avatar_hair;
+	public static uint avatar_top;
+	public static uint avatar_bottom;
+	public static uint avatar_accessories;
+	public static uint frame;
+
+	public static ArrayList unlocked_hair;
+	public static ArrayList unlocked_top;
+	public static ArrayList unlocked_bottom;
+	public static ArrayList unlocked_accessories;
+	public static ArrayList unlocked_frame;
+
+	public static uint money;
+	public static uint gems;
+
+	public static uint item_moneybooster;
+	public static uint item_removepair;
+	public static uint item_timebooster;
+
+	public static byte volume_music;
+	public static byte volume_sfx;
+
+	public static bool notification_on;
+
+}
