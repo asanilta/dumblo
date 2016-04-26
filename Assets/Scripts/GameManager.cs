4bc3e6954b39095dbf2abd4b690ce95b02598cab
bayuRP <13513046@std.stei.itb.ac.id>
2016-04-27 01:08:52 +0700

    minor changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,11 @@ public class GameManager : MonoBehaviour {
 		GlobalData.player_name = PlayerPrefs.GetString ("player_name", "Player");
 		GlobalData.player_exp = (uint) (int) PlayerPrefs.GetInt ("player_exp", 0);
 		GlobalData.player_level = (uint) (int) PlayerPrefs.GetInt ("player_level", 0);
+		GlobalData.money = (uint) (int) PlayerPrefs.GetInt ("money", 0);
+		GlobalData.gems = (uint) (int) PlayerPrefs.GetInt ("gems", 0);
+		GlobalData.item_moneybooster = (uint) (int) PlayerPrefs.GetInt ("item_moneybooster", 0);
+		GlobalData.item_removepair = (uint) (int) PlayerPrefs.GetInt ("item_removepair", 0);
+		GlobalData.item_timebooster = (uint) (int) PlayerPrefs.GetInt ("item_timebooster", 0);
 	}
 	
 	// Update is called once per frame
@@ -17,5 +22,9 @@ public class GameManager : MonoBehaviour {
 
 	void Awake() {
 		DontDestroyOnLoad(transform.gameObject);
+	}
+
+	uint NextExp(uint level) {
+		return (level + 1) * 100;
 	}
 }
