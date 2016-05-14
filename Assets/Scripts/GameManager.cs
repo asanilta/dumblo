9218e440f04fcddbe40e001c8925e4d3576738d6
bayuRP <13513046@std.stei.itb.ac.id>
2016-05-14 12:01:51 +0700

    moved variable initializations

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,14 +5,14 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GlobalData.player_name = PlayerPrefs.GetString ("player_name", "Player");
-		GlobalData.player_exp = (uint) (int) PlayerPrefs.GetInt ("player_exp", 0);
-		GlobalData.player_level = (uint) (int) PlayerPrefs.GetInt ("player_level", 0);
-		GlobalData.money = (uint) (int) PlayerPrefs.GetInt ("money", 0);
-		GlobalData.gems = (uint) (int) PlayerPrefs.GetInt ("gems", 0);
-		GlobalData.item_moneybooster = (uint) (int) PlayerPrefs.GetInt ("item_moneybooster", 0);
-		GlobalData.item_removepair = (uint) (int) PlayerPrefs.GetInt ("item_removepair", 0);
-		GlobalData.item_timebooster = (uint) (int) PlayerPrefs.GetInt ("item_timebooster", 0);
+		//GlobalData.player_name = PlayerPrefs.GetString ("player_name", "Player");
+		//GlobalData.player_exp = (uint) (int) PlayerPrefs.GetInt ("player_exp", 0);
+		//GlobalData.player_level = (uint) (int) PlayerPrefs.GetInt ("player_level", 1);
+		//GlobalData.money = (uint) (int) PlayerPrefs.GetInt ("money", 0);
+		//GlobalData.gems = (uint) (int) PlayerPrefs.GetInt ("gems", 0);
+		//GlobalData.item_moneybooster = (uint) (int) PlayerPrefs.GetInt ("item_moneybooster", 0);
+		//GlobalData.item_removepair = (uint) (int) PlayerPrefs.GetInt ("item_removepair", 0);
+		//GlobalData.item_timebooster = (uint) (int) PlayerPrefs.GetInt ("item_timebooster", 0);
 	}
 	
 	// Update is called once per frame
@@ -25,7 +25,7 @@ public class GameManager : MonoBehaviour {
 	}
 
 	public static uint NextExp(uint level) {
-		return (level + 1) * 100;
+		return (uint)((level) * 100 + (level - 1) * Mathf.Pow(50, level - 1));
 	}
 
 	public static void UpdateMoney() {
